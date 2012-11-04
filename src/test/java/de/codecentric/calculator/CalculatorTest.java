57c5a2bb1c74143e215faeea7b99acbd9937e068
max.hartmann <max.hartmann@codecentric.de>
2012-11-04 23:43:44 +0100

    changed test
    Change-Id: If9dba7e4dab9e4e31de308d70dbc572f35871033

--- src/test/java/de/codecentric/calculator/CalculatorTest.java
+++ src/test/java/de/codecentric/calculator/CalculatorTest.java
@@ -33,11 +33,11 @@ public class CalculatorTest {
 	@Test
 	public void shouldDisplayTheNumber() {
 		//Given
-		int numberToDisplay = 8;
+		int numberToDisplay = 9;
 		BDDMockito.given(labelMock.getText()).willReturn("0");
 		//When
 		calculator.addDigitToDisplay(numberToDisplay);
 		//Then
-		verify(labelMock).setText("8");
+		verify(labelMock).setText("9");
 	}
 }
