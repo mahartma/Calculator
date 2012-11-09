5711602ad1ebbddc98bfddd02053e7aa74d0df42
max.hartmann <max.hartmann@codecentric.de>
2012-11-09 13:13:46 +0100

    hook-test4

--- src/test/java/de/codecentric/calculator/CalculatorTest.java
+++ src/test/java/de/codecentric/calculator/CalculatorTest.java
@@ -33,11 +33,11 @@ public class CalculatorTest {
 	@Test
 	public void shouldDisplayTheNumber() {
 		//Given
-		int numberToDisplay = 14;
+		int numberToDisplay = 15;
 		BDDMockito.given(labelMock.getText()).willReturn("0");
 		//When
 		calculator.addDigitToDisplay(numberToDisplay);
 		//Then
-		verify(labelMock).setText("14");
+		verify(labelMock).setText("15");
 	}
 }
