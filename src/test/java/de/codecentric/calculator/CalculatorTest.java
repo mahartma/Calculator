f25e5311f2ab2486273199de202a452c55ea0c76
max.hartmann <max.hartmann@codecentric.de>
2012-11-09 09:37:49 +0100

    test changed

--- src/test/java/de/codecentric/calculator/CalculatorTest.java
+++ src/test/java/de/codecentric/calculator/CalculatorTest.java
@@ -33,11 +33,11 @@ public class CalculatorTest {
 	@Test
 	public void shouldDisplayTheNumber() {
 		//Given
-		int numberToDisplay = 10;
+		int numberToDisplay = 11;
 		BDDMockito.given(labelMock.getText()).willReturn("0");
 		//When
 		calculator.addDigitToDisplay(numberToDisplay);
 		//Then
-		verify(labelMock).setText("10");
+		verify(labelMock).setText("11");
 	}
 }
